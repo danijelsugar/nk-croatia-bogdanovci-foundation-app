010b679baecde5a2c634af767996c9d679368546
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-19 01:02:41 +0200

    Meta tags and css

---
+++ template/head.php
@@ -0,0 +1,15 @@
+<!-- META TAGS AND TITLE -->
+<meta charset="utf-8">
+<title><?php echo $appName; ?></title>
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta property="og:title" content="Nk Croatia Bogdanovci">
+<meta property="og:type" content="website">
+<meta property="og:description" content="
+A small local club with great history from the east of Croatia">
+<!-- FOUNDATION CSS -->
+<link rel="stylesheet" href="<?php echo $pathAPP; ?>/css/foundation.css">
+<!-- FONT AWESOME -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+<!-- CUSTOM CSS -->
+<link rel="stylesheet" href="<?php echo $pathAPP; ?>/css/app.css">
