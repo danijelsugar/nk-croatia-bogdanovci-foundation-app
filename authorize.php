baa74051713f7e317d63f9d6ba69e600ebe97495
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-19 00:55:59 +0200

    Authorization

---
+++ authorize.php
@@ -0,0 +1,19 @@
+<?php 
+
+	if(!isset($_POST["user"])){
+		exit;
+	}
+
+	include_once "configuration.php";
+
+	if($_POST["user"]===""){
+		header("location: login.php?message=2");
+		exit;
+	}
+
+	if($_POST["user"]==="dsugar" && $_POST["password"]==="d"){
+		$_SESSION["a"]=$_POST["user"];
+		header("location: private/dashboard.php");
+	}else {
+		header("location: login.php?message=1");
+	}
