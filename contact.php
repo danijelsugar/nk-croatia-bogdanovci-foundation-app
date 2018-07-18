fbea1f5b0388002e27bac856f3a2e1ec1f0808e5
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-19 00:58:30 +0200

    Contact page

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php include_once "configuration.php"; ?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+  <?php include_once "template/head.php"; ?>
+</head>
+<body>
+
+
+  <?php include_once "template/navbar.php"; ?>
+  Kontakt
+  <?php include_once "template/footer.php"; ?>
+
+  <?php include_once "template/scripts.php"; ?>
+</body>
+</html>
