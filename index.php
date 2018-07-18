d2870e7050d135f63621ebedbed65cb304534fe0
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-19 00:58:44 +0200

    Home page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include_once "configuration.php"; ?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+  <?php include_once "template/head.php"; ?>
+</head>
+<body>
+
+
+  <?php include_once "template/navbar.php"; ?>
+  <div class="grid-container full">
+    <div class="grid-x">
+      <div class="cell small-12 medium-12 large-12 text-center">
+        <div class="background-img">
+         
+       </div>
+     </div>
+   </div>
+  </div>
+  <?php include_once "template/footer.php"; ?>
+
+  <?php include_once "template/scripts.php"; ?>
+</body>
+</html>
