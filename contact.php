1782c61a4d1b8c9c0ab8e5c3c5cc9c99680a43c6
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-20 20:15:09 +0200

    Implemented google maps

--- contact.php
+++ contact.php
@@ -8,7 +8,25 @@
 
 
   <?php include_once "template/navbar.php"; ?>
-  Kontakt
+  
+  <div class="grid-container">
+  	<h1>My map</h1>
+  	<div class="grid-x">
+  		<div id="googleMap" style="width: 100%; height: 400px;"></div>
+  		<script>
+  			//google map function
+  			function myMap() {
+  				var mapCenter = new google.maps.LatLng(45.342800,18.928486);
+  				var mapCanvas = document.getElementById("googleMap");
+  				var mapOptions = {center: mapCenter, zoom: 15};
+  				var map = new google.maps.Map(mapCanvas, mapOptions);
+  				var marker = new google.maps.Marker({position: mapCenter});
+  				marker.setMap(map);
+  			}
+  		</script>
+  	</div>
+  </div>
+
   <?php include_once "template/footer.php"; ?>
 
   <?php include_once "template/scripts.php"; ?>
