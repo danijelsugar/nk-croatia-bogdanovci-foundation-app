d81c9305f5ffecccb2f2efe8ecb93bfbdbc384e2
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-23 00:30:57 +0200

    Included google maps api

--- template/scripts.php
+++ template/scripts.php
@@ -3,3 +3,6 @@
 <script src="<?php echo $pathAPP; ?>/js/vendor/what-input.js"></script>
 <script src="<?php echo $pathAPP; ?>/js/vendor/foundation.js"></script>
 <script src="<?php echo $pathAPP; ?>/js/app.js"></script>
+ <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2-OWWKXPagI2nZVi-uF7msqbfi0aYtqw&callback=myMap">
+    </script>
