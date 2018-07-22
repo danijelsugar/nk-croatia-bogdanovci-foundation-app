8e4bb063e72085b8b76358a74308e267935215d9
Danijel Sugar <danijel.sugar@outlook.com>
2018-07-23 00:27:08 +0200

    Aded link to github code

--- template/footer.php
+++ template/footer.php
@@ -1,4 +1,6 @@
-<div class="grid-container">
+<div class="grid-container text-center">
 	<hr />
+	<i class="fab fa-github fa-2x"></i><a href="https://github.com/danijelsugar/nk-croatia-bogdanovci-foundation-app">GitHub Code</a><br />
+	<i class="fas fa-project-diagram fa-2x"></i><a href="">ERA diagram</a><br />
 	&copy; <?php echo date("Y") ?> Nk Croatia Bogdanovci-Danijel Šugar
 </div>
