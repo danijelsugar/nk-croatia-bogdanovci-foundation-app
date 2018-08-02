c0a56671ab29f7011489e468a6a7c07b5c69a51e
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-02 19:55:43 +0200

    New navbar

--- template/navbar.php
+++ template/navbar.php
@@ -1,36 +1,33 @@
-<nav class="top-bar topbar-responsive">
-  <div class="top-bar-title">
-    <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
+<div class="grid-container">
+  <div class="grid-x">
+    <div class="cell">
+     <div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
       <button class="menu-icon" type="button" data-toggle></button>
-    </span>
-    <a class="topbar-responsive-logo" href="#"><img src="https://placehold.it/100x39" alt="" /></a>
-  </div>
-  <div id="topbar-responsive" class="topbar-responsive-links">
-    <div class="top-bar-right">
-      <ul class="menu simple vertical medium-horizontal">
+      <div class="title-bar-title">Menu</div>
+    </div>
+
+    <div class="top-bar align-center" id="main-menu">
+      <ul class="menu vertical medium-horizontal medium-text-center" data-responsive-menu="drilldown medium-dropdown">
         <?php
-          menuItem($pathAPP, "index.php", "Naslovna");
-          menuItem($pathAPP, "contact.php", "Kontakt");
+        menuItem($pathAPP, "index.php", "Naslovna");
+        menuItem($pathAPP, "contact.php", "Kontakt");
 
 
-          if(isset($_SESSION["a"])):
-            menuItem($pathAPP, "private/dashboard.php", "Nadzorna ploča");
-            menuItem($pathAPP, "private/club/index.php", "Klubovi");
-            menuItem($pathAPP, "private/coach/index.php", "Treneri");
-          endif;
+        if(isset($_SESSION["a"])):
+          menuItem($pathAPP, "private/dashboard.php", "Nadzorna ploča");
+          menuItem($pathAPP, "private/club/index.php", "Klubovi");
+          menuItem($pathAPP, "private/coach/index.php", "Treneri");
+          menuItem($pathAPP, "private/player/index.php", "Igrači");
+        endif;
+
+        if(isset($_SESSION["a"])): 
+          menuItem($pathAPP, "logout.php", "Logout");
+        else: 
+          menuItem($pathAPP, "login.php", "Login");
+        endif; 
         ?>
-
-        <?php 
-          if(isset($_SESSION["a"])): 
-            menuItem($pathAPP, "logout.php", "Logout");
-          else: 
-            menuItem($pathAPP, "login.php", "Login");
-          endif; 
-        ?>
-            <li>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
