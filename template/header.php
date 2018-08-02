ac74221a4e4ae4e7cb45e8c033ab775f9bef5d73
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-02 19:59:23 +0200

    Header with logo

---
+++ template/header.php
@@ -0,0 +1,10 @@
+<div class="grid-container text-center">
+    <div class="grid-x grid-padding-y">
+      <div class="cell large-6 logo">
+          <img src="<?php echo $pathAPP; ?>/img/cro-grb.png" width="150px;" height="150px;" alt="">
+      </div>
+      <div class="cell large-6 logo-text">
+          <p>CROATIA BOGDANOVCI<br><span>Službena stranica</span></p>
+      </div>
+    </div>
+  </div>
