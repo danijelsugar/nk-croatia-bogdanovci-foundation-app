53d25112608a833d0adfdc4b8cd36d8d43afe463
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-02 19:54:52 +0200

    Open Graph Protocol meta tags-not completed

--- template/head.php
+++ template/head.php
@@ -5,11 +5,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta property="og:title" content="Nk Croatia Bogdanovci">
 <meta property="og:type" content="website">
-<meta property="og:description" content="
-A small local club with great history from the east of Croatia">
+<meta property="og:url" content="http://dsugar.byethost16.com">
+<meta property="og:image" content="">
+<meta property="og:image:type" content="image/jpg">
+<meta proprety="og:image:width" content="300">
+<meta proprety="og:image:height" content="300">
+<meta property="og:image:alt" content="Nk Croatia Bogdanovci logo"> 
+<meta property="og:description" content="Nk Croatia Bogdanovci is a small football club of rich history from the east of Croatia">
+<!-- FAVICON -->
+<link rel="shortcut icon" href="<?php echo $pathAPP; ?>/img/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php echo $pathAPP; ?>/img/favicon.ico" type="image/x-icon">
 <!-- FOUNDATION CSS -->
 <link rel="stylesheet" href="<?php echo $pathAPP; ?>/css/foundation.css">
 <!-- FONT AWESOME -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 <!-- CUSTOM CSS -->
 <link rel="stylesheet" href="<?php echo $pathAPP; ?>/css/app.css">
