ea44619451eb9450e17452b252def3ab041aca32
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-02 19:58:57 +0200

    Authorized person can CRUD data of player

---
+++ private/player/index.php
@@ -0,0 +1,64 @@
+<?php include_once "../../configuration.php"; 
+if(!isset($_SESSION["a"])){
+  header("location: " . $pathAPP . "logout.php");
+}
+
+?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+  <?php include_once "../../template/head.php"; ?>
+</head>
+<body>
+  <?php include_once "../../template/header.php" ?>
+
+  <?php include_once "../../template/navbar.php"; ?>
+
+  <?php
+  $query = $connect->prepare("select * from igrac;");
+  $query->execute();
+  $result = $query->fetchAll(PDO::FETCH_OBJ);
+  ?>
+
+  <div class="grid-container">
+    <div class="grid-x">
+      <h3>Igrači</h3>
+      <hr />
+      <a href="new.php" class="button expanded">Dodaj igrača</a>
+      <table class="stack">
+        <thead>
+          <th>Ime</th>
+          <th>Prezime</th>
+          <th>Oib</th>
+          <th>Broj telefona</th>
+          <th>Registracijski broj</th>
+          <th>Akcija</th>
+        </thead>
+        <tbody>
+          <?php foreach($result as $row): ?>
+            <tr>
+              <td><?php echo $row->ime ?></td>
+              <td><?php echo $row->prezime ?></td>
+              <td><?php echo $row->oib ?></td>
+              <td><?php echo $row->brojtelefona ?></td>
+              <td><?php echo $row->brojregistracije ?></td>
+              <td>
+                <a href="update.php?sifra=<?php echo $row->sifra; ?>">
+                  <i class="fas fa-edit fa-2x"></i>
+                </a>
+                <a href="delete.php?sifra=<?php echo $row->sifra; ?>">
+                  <i class="fas fa-trash fa-2x"></i>
+                </a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <?php include_once "../../template/footer.php"; ?>
+
+  <?php include_once "../../template/scripts.php"; ?>
+</body>
+</html>
