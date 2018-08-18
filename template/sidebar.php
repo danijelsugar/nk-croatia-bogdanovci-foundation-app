7d1d0766614e2e6bbaf60caf40b5013d9df52620
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-18 19:22:01 +0200

    Sidebar for dashboard

---
+++ template/sidebar.php
@@ -0,0 +1,15 @@
+<div class="cell large-2 medium-3 hidden-for-small">
+	<div class="title-bar" data-responsive-toggle="side-menu" data-hide-for="medium">
+		<button class="menu-icon" type="button" data-toggle="side-menu"></button>
+		<div class="title-bar-title">Menu</div>
+	</div>
+	<div id="side-menu" class="side-nav">
+		<ul class="vertical menu">
+			<li class="menu-text"><a href="<?php echo $pathAPP; ?>/private/dashboard.php">Nadzorna Ploča</a></li>
+			<li><a href="<?php echo $pathAPP; ?>/private/club/index.php">Klubovi</a></li>
+			<li><a href="<?php echo $pathAPP; ?>/private/coach/index.php"><i class="fas fa-whistle"></i>Treneri</a></li>
+			<li><a href="<?php echo $pathAPP; ?>/private/player/index.php">Igrači</a></li>
+			<li><a href="<?php echo $pathAPP; ?>/logout.php">Logout</a></li>
+		</ul>
+	</div>
+</div>
