352d3d81e99507f77d899762763af04b4e195f75
Danijel Sugar <danijel.sugar@outlook.com>
2018-08-18 20:03:14 +0200

    New path for byethost

--- configuration.php
+++ configuration.php
@@ -10,10 +10,23 @@
 		    $pathAPP="/nk_croatia_bogdanovci_app/Site/";
 	    break;
 	    case "dsugar.byethost16.com":
-		    $pathAPP="/nk_croatia_bogdanovci_app/";
+		    $pathAPP="/Site/";
 	    break;
 }
-
-
+/*
+	==============================
+		Byet host
+	==============================
+*/
+/*
+	$connect = new PDO("mysql:host=sql304.byethost.com;port=3306;dbname=b16_21955356_croatia","b16_21955356","bogibatina13");
+	$connect->exec("set names utf8;");
+*/
+/*
+	==============================
+		Local host
+	==============================
+*/
+	
 	$connect = new PDO("mysql:host=localhost;dbname=croatia","edunova","edunova");
 	$connect->exec("set names utf8;");
